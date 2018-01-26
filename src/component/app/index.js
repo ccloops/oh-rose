9caf72f5ed287a2f903c81f987d19962975de53f
Catherine Looper <loopercat@gmail.com>
2018-01-26 15:45:49 -0800

    Added css files and components

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,8 +1,11 @@
 import './_app.scss';
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 import Landing from '../landing';
+import Music from '../music';
+import Schedule from '../schedule';
+import Contact from '../contact';
 
 class App extends React.Component {
   render() {
@@ -10,7 +13,21 @@ class App extends React.Component {
       <div className='app'>
         <BrowserRouter>
           <div>
+            <header>
+              <h1>Oh, Rose</h1>
+              <nav>
+                <ul>
+                  <li><Link to='/'>Home</Link></li>
+                  <li><Link to='/music'>Music</Link></li>
+                  <li><Link to='/schedule'>Schedule</Link></li>
+                  <li><Link to='/contact'>Contact</Link></li>
+                </ul>
+              </nav>
+            </header>
             <Route exact path='/' component={Landing}/>
+            <Route exact path='/music' component={Music}/>
+            <Route exact path='/schedule' component={Schedule}/>
+            <Route exact path='/contact' component={Contact}/>
           </div>
         </BrowserRouter>
       </div>
--- src/component/landing/index.js
+++ src/component/landing/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 // import {Link} from 'react-router-dom';
-// import {connect} from 'react-redux';
 
 class Landing extends React.Component {
-  constructor(props) {
-    super(props);
-
-    let memberFunctions = Object.getOwnPropertyNames(Landing.prototype);
-    for(let functionName of memberFunctions) {
-      if(functionName.startsWith('handle')) {
-        this[functionName] = this[functionName].bind(this);
-      }
-    }
-  }
-
   render() {
-    
     return (
-      <h1>Oh, Rose</h1>
+      <div className='landing'>
+        <h1>Oh, Rose</h1>
+      </div>
     );
   }
 }
