2a359daa07a5ce96e94189c49957915e56e76f82
Catherine Looper <loopercat@gmail.com>
2018-01-28 11:12:56 -0800

    Added baskerville font, and contact component

---
+++ src/component/contact/index.js
@@ -0,0 +1,12 @@
+import './_contact.scss';
+import React from 'react';
+class Contact extends React.Component {
+  render() {
+    return(
+      <div className='music'>
+      </div>
+    );
+  }
+}
+
+export default Contact;
