291f78878f28f37e5dc5f4db76a033812825313e
Catherine Looper <loopercat@gmail.com>
2018-01-29 14:24:55 -0800

    Changed dist to build

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 express()
   .use(express.static(`${__dirname}/dist`))
-  .get('*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
+  .get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`))
   .listen(process.env.PORT, () =>
     console.log('__SERVER_RUNNING__', process.env.PORT));
