b116f5db69e9dfc03b4d20d7b9b1e4233c3f61ca
Catherine Looper <loopercat@gmail.com>
2018-01-29 14:22:51 -0800

    Added an index.js file in the root

--- src/main.js
+++ src/main.js
@@ -2,15 +2,8 @@ import './style/main.scss';
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './component/app';
-const express = require('express');
-const app = express();
 
 const container = document.createElement('div');
 document.body.appendChild(container);
 
 ReactDom.render(<App/>, container);
-
-
-app.get('/', (req, res) => res.send('build/index.html'));
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
