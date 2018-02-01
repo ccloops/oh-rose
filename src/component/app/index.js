61ef930723ecd286711d9bfd98f5a49b7f0a9416
Catherine Looper <loopercat@gmail.com>
2018-01-31 17:10:01 -0800

    Updating background style - removed seven

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -8,6 +8,12 @@ import Photo from '../photo';
 import Video from '../video';
 import Schedule from '../schedule';
 import Contact from '../contact';
+import tattoos from '../../assets/tattoos.jpg';
+// import flowerBackground from '../../assets/flower-background.JPG';
+import flowers from '../../assets/flowers.jpg';
+import sunshineRoses from '../../assets/sunshine-roses.jpg';
+import bedPic from '../../assets/bed-pic.jpg';
+
 
 class App extends React.Component {
   render() {
@@ -27,13 +33,24 @@ class App extends React.Component {
               </nav>
               <h1>Oh, Rose</h1>
             </header>
+            <main>
+              <div className='triangle1'>
+                <img src={sunshineRoses}/>
+              </div>
+              <div className = 'triangle2'>
+                <img src={flowers}/>
+              </div>
+              <div className = 'triangle3'>
+                <img src={bedPic}/>
+              </div>
+            </main>
             <footer>
               <ul className='icons'>
-                <li><a href='https://www.instagram.com/ohroseh/' target='blank'><FaInstagram/></a></li>
                 <li><a href='https://www.facebook.com/ohrosemusic/' target='blank'><FaFacebookSquare/></a></li>
-                <li><a href='https://twitter.com/ohrosemusic?lang=en' target='blank'><FaTwitter/></a></li>
                 <li><a href='https://ohrose.bandcamp.com/' target='blank'><FaMusic/></a></li>
                 <li><a href='https://open.spotify.com/artist/3k7ErvhO8cmFVOTrAVtuKe' target='blank'><FaSpotify/></a></li>
+                <li><a href='https://www.instagram.com/ohroseh/' target='blank'><FaInstagram/></a></li>
+                <li><a href='https://twitter.com/ohrosemusic?lang=en' target='blank'><FaTwitter/></a></li>
               </ul>
             </footer>
             <Route exact path='/' component={Landing}/>
--- src/component/landing/index.js
+++ src/component/landing/index.js
@@ -10,6 +10,7 @@ class Landing extends React.Component {
   render() {
     return (
       <div className='landing'>
+        <div></div>
   
       </div>
     );
